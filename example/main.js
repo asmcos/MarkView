baab578195b93d0b9d7ae6077d7627fd0ff73ac7
asmcos <asmcos@blx.com>
2025-08-21 18:07:19 +0800

    开始一个新的项目markdown viewer

---
+++ example/main.js
@@ -0,0 +1,29 @@
+import { initMarkdownEditor } from '../src/utils/markdown.js';
+
+initMarkdownEditor({
+  editorId: 'editor',
+  previewId: 'preview',
+  theme: 'auto'
+}).then(render=>{
+  document.getElementById('editor').value = `# MarkView Demo
+
+[!NOTE] 这是 NOTE
+[!TIP] 这是 TIP
+[!IMPORTANT] 重要信息
+[!WARNING] 警告信息
+[!CAUTION] 注意事项
+
+:::tip
+Tip 容器
+:::
+
+:::warning
+Warning 容器
+:::
+
+\`\`\`js {2} ln
+console.log("Hello MarkView");
+\`\`\`
+`;
+  render();
+});
